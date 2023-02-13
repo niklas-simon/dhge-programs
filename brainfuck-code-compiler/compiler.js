6ac4981b575ab00a6e41565c2d03d0e52bf535ba
Niklas Simon Pein <niklaspein12@gmail.com>
2023-02-13 13:42:42 +0100

    bf-code-compiler

--- brainfuck-code-compiler/compiler.js
+++ brainfuck-code-compiler/compiler.js
@@ -10,12 +10,10 @@ const input = (await fs.readFile(process.argv[2])).toString("utf-8");
 let pointer = 0;
 
 const variables = [];
+const loopvars = [];
 
-const assignVariable = (match) => {
+const setPointerToVariable = (name) => {
     let output = "";
-
-    const name = match.groups.name;
-    const value = Number(match.groups.value);
     const existing = variables.findIndex(variable => variable === name);
 
     // get index of variable
@@ -23,7 +21,7 @@ const assignVariable = (match) => {
     if (existing !== -1) {
         index = existing;
     } else {
-        index = variables.push(name);
+        index = variables.push(name) - 1;
     }
 
     // set pointer to variable
@@ -32,8 +30,19 @@ const assignVariable = (match) => {
     } else {
         output += "<".repeat(pointer - index);
     }
-    output += "\n";
     pointer = index;
+
+    return output;
+}
+
+const assignVariable = (match) => {
+    let output = "";
+
+    const name = match.groups.name;
+    const value = Number(match.groups.value);
+    const existing = variables.findIndex(variable => variable === name);
+    
+    output += setPointerToVariable(name);
 
     if (existing !== -1) {
         // set variable to 0 if existing
@@ -46,13 +55,86 @@ const assignVariable = (match) => {
     return output;
 }
 
-const output = input.replace(/\s/g, "").split(/(?![;{}])/g).map(line => {
-    let newLine = "\n";
-    const matchVar = line.match(/\$(?<name>\w+)=(?<value>\d+);/);
-    if (matchVar) {
-        newLine += assignVariable(matchVar);
-    }
-    return newLine == "\n" ? line : newLine;
-}).join("\n");
+const startLoop = (match) => {
+    let output = "";
+    const name = match.groups.name;
+    const next = match.groups.next;
 
-console.log(output);
+    loopvars.push(name);
+    output += setPointerToVariable(name);
+
+    output += "[\n";
+    output += compileLine(next);
+
+    return output;
+}
+
+const endLoop = (match) => {
+    let output = "";
+    const next = match.groups.next;
+    const name = loopvars[loopvars.length - 1];
+
+    output += setPointerToVariable(name);
+    loopvars.splice(loopvars.length - 1, 1);
+
+    output += "]\n";
+    output += compileLine(next);
+
+    return output;
+}
+
+const incrementVariable = (match) => {
+    let output = "";
+    const name = match.groups.name;
+    const sign = match.groups.sign;
+    const value = Number(match.groups.value);
+
+    output += setPointerToVariable(name);
+    output += sign.repeat(value);
+
+    return output;
+}
+
+const variableInOut = (match) => {
+    let output = "";
+    const mode = match.groups.mode;
+    const name = match.groups.name;
+
+    output += setPointerToVariable(name);
+    output += match.groups.mode === "in" ? "," : ".";
+
+    return output;
+}
+
+const compileLine = (line) => {
+    let newLine = "";
+    const matchAssign = line.match(/^\$(?<name>\w+)=(?<value>\d+)$/);
+    if (matchAssign) {
+        newLine += assignVariable(matchAssign);
+    }
+    const matchLoopStart = line.match(/^loop\(\$(?<name>\w+)\)\{(?<next>.+)$/);
+    if (matchLoopStart) {
+        newLine += startLoop(matchLoopStart);
+    }
+    const matchLoopEnd = line.match(/^\}(?<next>.+)$/);
+    if (matchLoopEnd) {
+        newLine += endLoop(matchLoopEnd);
+    }
+    const matchIncrement = line.match(/^\$(?<name>\w+)(?<sign>[+-])(?<value>\d+)$/);
+    if (matchIncrement) {
+        newLine += incrementVariable(matchIncrement);
+    }
+    const matchInOut = line.match(/^(?<mode>in|out)\(\$(?<name>\w+)\)$/);
+    if (matchInOut) {
+        newLine += variableInOut(matchInOut);
+    }
+    return (newLine === "" ? line : newLine);
+}
+
+const output = input
+    .replace(/\s/g, "")
+    .split(";")
+    .map(compileLine)
+    .join("\n");
+
+await fs.writeFile("out.bf", output);
