b6bccd982f87dc602b799922e5cb8de2385a1041
Niklas <niklaspein12@gmail.com>
2023-02-22 11:15:36 +0000

    magic-square-finder

--- magic-square-finder/finder.c
+++ magic-square-finder/finder.c
@@ -2,13 +2,25 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#define IS 0
+#define COULD_BE 1
+#define IS_NOT 2
+
+int found = 0;
+int length = 0;
+int length_2 = 0;
+int realsum = 0;
 
 void get_out() {
     printf("usage: finder.out length");
     exit(0);
 }
 
-void print(int **square, int length) {
+void print(int **square) {
     for (int i = 0; i < length; i++) {
         for (int j = 0; j < length; j++) {
             if (j) {
@@ -18,11 +30,12 @@ void print(int **square, int length) {
         }
         printf("\n");
     }
+    printf("\n");
 }
 
-void check(int **square, int length) {
-    int realsum = length * (length * length + 1) / 2;
-    printf("All sums must be %i...\n", realsum);
+int check(int **square) {
+    bool success = true;
+    bool is_null = false;
 
     // check rows and cols
     for (int i = 0; i < length; i++) {
@@ -31,14 +44,15 @@ void check(int **square, int length) {
         for (int j = 0; j < length; j++) {
             rowsum += square[i][j];
             colsum += square[j][i];
+            if (!square[i][j] || !square[j][i]) {
+                is_null = true;
+            }
         }
-        if (rowsum != realsum) {
-            printf("... but the sum of row %i is %i.\n", i, rowsum);
-            return;
+        if (rowsum > realsum || colsum > realsum) {
+            return IS_NOT;
         }
-        if (colsum != realsum) {
-            printf("... but the sum of column %i is %i.\n", i, colsum);
-            return;
+        if (rowsum != realsum || colsum != realsum) {
+            success = false;
         }
     }
 
@@ -49,42 +63,50 @@ void check(int **square, int length) {
         mainsum += square[i][i];
         secsum += square[length - i - 1][i];
     }
-    if (mainsum != realsum) {
-        printf("... but the sum of the main diagonal is %i.\n", mainsum);
-        return;
+    if (mainsum > realsum || secsum > realsum) {
+        return IS_NOT;
     }
-    if (secsum != realsum) {
-        printf("... but the sum of the secondary diagonal is %i.\n", secsum);
-        return;
+    if (mainsum != realsum || secsum != realsum) {
+        success = false;
     }
 
-    printf("... and they oh so definitely are!\n");
+    return !is_null && success ? IS : COULD_BE;
 }
 
-void generate(int **square, int length) {
-    int x = 0; int y = 0;
-    for (int i = 1; i <= length * length; i++) {
-        square[x][y] = i;
-        if (i % length) {
-            x = (x + length - 1) % length;
-            y = (y + 2) % length;
+void generate(int **square, bool *available, int x, int y) {
+    for (int i = 0; i < length_2; i++) {
+        if (available[i]) {
+            available[i] = false;
         } else {
-            x = (x + length - 2) % length;
+            continue;
         }
-    }
 
-    print(square, length);
-    check(square, length);
+        square[x][y] = i + 1;
+        switch (check(square)) {
+            case IS:
+                print(square);
+                found++;
+                break;
+            case COULD_BE:
+                int new_x = x + 1;
+                int new_y = y;
+                if (new_x >= length) {
+                    new_x = 0;
+                    new_y++;
+                    if (new_y >= length) {
+                        break;
+                    }
+                }
+                generate(square, available, new_x, new_y);
+                break;
+        }
+        available[i] = true;
+    }
+    square[x][y] = 0;
 }
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        get_out();
-    }
-    int length = atoi(argv[1]);
-    if (length < 0 || length % 2 == 0 || length % 3 == 0) {
-        get_out();
-    }
+void *thread(void *args) {
+    int index = *((int *)(args));
 
     int **square = (int **)malloc(length * sizeof(int *));
     for (int i = 0; i < length; i++) {
@@ -97,6 +119,43 @@ int main(int argc, char **argv) {
             square[i][j] = 0;
         }
     }
+    bool *available = (bool *)malloc(length_2 * sizeof(bool));
+    for (int i = 0; i < length_2; i++) {
+        available[i] = true;
+    }
 
-    generate(square, length);
+    available[index] = false;
+    square[0][0] = index + 1;
+
+    generate(square, available, 1, 0);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        get_out();
+    }
+    length = atoi(argv[1]);
+    if (length < 3) {
+        get_out();
+    }
+    length_2 = length * length;
+    realsum = length * (length_2 + 1) / 2;
+    
+    pthread_t threads[length_2];
+    int indices[length_2];
+
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < length_2; i++) {
+        indices[i] = i;
+        pthread_create(&(threads[i]), NULL, thread, &(indices[i]));
+    }
+
+    for (int i = 0; i < length_2; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    gettimeofday(&stop, NULL);
+    printf("found %i squares in %f ms\n", found, (double)((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec) / 1000); 
 }
