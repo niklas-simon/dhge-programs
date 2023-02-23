fe63a18d5b6f42442a904ba746af9b3501e2fe19
Niklas <niklaspein12@gmail.com>
2023-02-23 14:54:46 +0000

    magic-square

--- magic-square-finder/finder.c
+++ magic-square-finder/finder.c
@@ -4,7 +4,11 @@
 #include <string.h>
 #include <stdbool.h>
 #include <sys/time.h>
-#include <pthread.h>
+#ifdef __linux__
+    #include <pthread.h>
+#else
+    #include <windows.h>
+#endif
 
 #define IS 0
 #define COULD_BE 1
@@ -21,65 +25,127 @@ void get_out() {
 }
 
 void print(int **square) {
+    char string[128];
+    char *ptr = string;
     for (int i = 0; i < length; i++) {
         for (int j = 0; j < length; j++) {
             if (j) {
-                printf(" ");
+                *ptr = ' ';
+                ptr++;
             }
-            printf("%2i", square[i][j]);
+            ptr += sprintf(ptr, "%2i", square[i][j]);
         }
-        printf("\n");
+        *ptr = '\n';
+        ptr++;
     }
-    printf("\n");
+    *ptr = '\n';
+    ptr++;
+    *ptr = '\0';
+    fputs(string, stdout);
 }
 
 int check(int **square) {
     bool success = true;
-    bool is_null = false;
 
-    // check rows and cols
+    // check rows
     for (int i = 0; i < length; i++) {
-        int rowsum = 0;
-        int colsum = 0;
+        bool is_null = false;
+        int sum = 0;
         for (int j = 0; j < length; j++) {
-            rowsum += square[i][j];
-            colsum += square[j][i];
-            if (!square[i][j] || !square[j][i]) {
+            sum += square[i][j];
+            if (!square[i][j]) {
                 is_null = true;
             }
         }
-        if (rowsum > realsum || colsum > realsum) {
+        if (sum > realsum) {
             return IS_NOT;
         }
-        if (rowsum != realsum || colsum != realsum) {
+        if (sum != realsum) {
+            if (!is_null) {
+                return IS_NOT;
+            }
             success = false;
         }
     }
 
-    // check diags
-    int mainsum = 0;
-    int secsum = 0;
+    // check cols
     for (int i = 0; i < length; i++) {
-        mainsum += square[i][i];
-        secsum += square[length - i - 1][i];
+        bool is_null = false;
+        int sum = 0;
+        for (int j = 0; j < length; j++) {
+            sum += square[j][i];
+            if (!square[j][i]) {
+                is_null = true;
+            }
+        }
+        if (sum > realsum) {
+            return IS_NOT;
+        }
+        if (sum != realsum) {
+            if (!is_null) {
+                return IS_NOT;
+            }
+            success = false;
+        }
     }
-    if (mainsum > realsum || secsum > realsum) {
+
+    // check main diag
+    int sum = 0;
+    bool is_null = false;
+    for (int i = 0; i < length; i++) {
+        sum += square[i][i];
+        if (!square[i][i]) {
+            is_null = true;
+        }
+    }
+    if (sum > realsum) {
         return IS_NOT;
     }
-    if (mainsum != realsum || secsum != realsum) {
+    if (sum != realsum) {
+        if (!is_null) {
+            return IS_NOT;
+        }
         success = false;
     }
 
-    return !is_null && success ? IS : COULD_BE;
+    // check secondary diag
+    sum = 0;
+    is_null = false;
+    for (int i = 0; i < length; i++) {
+        sum += square[length - i - 1][i];
+        if (!square[i][i]) {
+            is_null = true;
+        }
+    }
+    if (sum > realsum) {
+        return IS_NOT;
+    }
+    if (sum != realsum) {
+        if (!is_null) {
+            return IS_NOT;
+        }
+        success = false;
+    }
+
+    return success ? IS : COULD_BE;
 }
 
 void generate(int **square, bool *available, int x, int y) {
+    bool proceed = true;
+    int new_x = x + 1;
+    int new_y = y;
+    if (new_x >= length) {
+        new_x = 0;
+        new_y++;
+        if (new_y >= length) {
+            proceed = false;
+        }
+    }
     for (int i = 0; i < length_2; i++) {
-        if (available[i]) {
-            available[i] = false;
-        } else {
+        if (!available[i]) {
             continue;
         }
+        available[i] = false;
 
         square[x][y] = i + 1;
         switch (check(square)) {
@@ -88,24 +154,24 @@ void generate(int **square, bool *available, int x, int y) {
                 found++;
                 break;
             case COULD_BE:
-                int new_x = x + 1;
-                int new_y = y;
-                if (new_x >= length) {
-                    new_x = 0;
-                    new_y++;
-                    if (new_y >= length) {
-                        break;
-                    }
+                if (!proceed) {
+                    break;
                 }
                 generate(square, available, new_x, new_y);
                 break;
         }
+
         available[i] = true;
     }
     square[x][y] = 0;
 }
 
-void *thread(void *args) {
+#ifdef __linux__
+    void *thread(void *args)
+#else
+    DWORD WINAPI thread(LPVOID args)
+#endif
+{
     int index = *((int *)(args));
 
     int **square = (int **)malloc(length * sizeof(int *));
@@ -141,7 +207,11 @@ int main(int argc, char **argv) {
     length_2 = length * length;
     realsum = length * (length_2 + 1) / 2;
     
-    pthread_t threads[length_2];
+    #ifdef __linux__
+        pthread_t threads[length_2];
+    #else
+        HANDLE threads[length_2];
+    #endif
     int indices[length_2];
 
     struct timeval stop, start;
@@ -149,13 +219,29 @@ int main(int argc, char **argv) {
 
     for (int i = 0; i < length_2; i++) {
         indices[i] = i;
-        pthread_create(&(threads[i]), NULL, thread, &(indices[i]));
+        #ifdef __linux__
+            pthread_create(&(threads[i]), NULL, thread, &(indices[i]));
+        #else
+            threads[i] = CreateThread(NULL, 0, thread, &(indices[i]), 0, NULL);
+        #endif
     }
 
-    for (int i = 0; i < length_2; i++) {
-        pthread_join(threads[i], NULL);
-    }
+    #ifdef __linux__
+        for (int i = 0; i < length_2; i++) {
+            pthread_join(threads[i], NULL);
+        }
+    #else
+        WaitForMultipleObjects(length_2, threads, TRUE, INFINITE);
+    #endif
 
     gettimeofday(&stop, NULL);
-    printf("found %i squares in %f ms\n", found, (double)((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec) / 1000); 
+    char time[16];
+    int diff_s = stop.tv_sec - start.tv_sec;
+    int diff_us = stop.tv_usec - start.tv_usec;
+    int hours = diff_s / 3600;
+    int minutes = (diff_s % 3600) / 60;
+    int seconds = diff_s % 60;
+    int milliseconds = diff_us / 1000;
+    sprintf(time, "%02d:%02d:%02d.%04dh", hours, minutes, seconds, milliseconds);
+    printf("found %i squares, took %s\n", found, time); 
 }
